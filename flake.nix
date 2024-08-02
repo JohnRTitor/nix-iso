cede3d9f8e32d48038b2bea05861c569aa6739c9
John Titor <50095635+JohnRTitor@users.noreply.github.com>
2024-08-02 15:41:16 +0530

    flake: add nix-community cache

--- flake.nix
+++ flake.nix
@@ -44,4 +44,14 @@
       inherit specialArgs;
     };
   };
+
+  # Add nix community cache
+  nixConfig = {
+    extra-substituters = [
+      "https://nix-community.cachix.org"
+    ];
+    extra-trusted-public-keys = [
+      "nix-community.cachix.org-1:mB9FSh9qf2dCimDSUo8Zy7bkq5CX+/rkCWyvRCYg3Fs="
+    ];
+  };
 }
